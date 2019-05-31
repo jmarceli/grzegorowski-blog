3082b6ab41afcac6613417b7f570970514a6f8d1
Jan Grzegorowski <grzegorowski@gmail.com>
2019-05-31 09:20:49 +0200

    Adjust typography

--- gatsby-theme-test/src/utils/typography.js
+++ gatsby-theme-test/src/utils/typography.js
@@ -1,6 +1,11 @@
 import Typography from "typography";
-import githubTheme from "typography-theme-github";
+// import githubTheme from "typography-theme-github";
 
-const typography = new Typography(githubTheme);
+// const typography = new Typography(githubTheme);
+const typography = new Typography({
+  baseFontSize: "21px",
+  baseLineHeight: 1.666,
+  headerWeight: 500,
+});
 
 export default typography;
