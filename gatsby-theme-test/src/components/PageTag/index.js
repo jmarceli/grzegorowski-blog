15b35bd334dbd21136ad96b90f744f63561c2748
Jan Grzegorowski <grzegorowski@gmail.com>
2019-06-13 13:41:53 +0200

    Clean PageTag code

--- gatsby-theme-test/src/components/PageTag/index.js
+++ gatsby-theme-test/src/components/PageTag/index.js
@@ -2,8 +2,7 @@ import React from "react";
 import CardList from "../CardList";
 import HomeHeader from "../HomeHeader";
 import PageLayout from "../PageLayout";
-import Menu from "../Menu";
-import { Header, Content } from "./styles";
+import { Content } from "./styles";
 
 export default function PageTag({ posts, tag, authors }) {
   const postsWithAuthors = posts.map(({ node }) => {
@@ -15,7 +14,6 @@ export default function PageTag({ posts, tag, authors }) {
       author: author && author.node,
     };
   });
-  console.log(posts, authors, postsWithAuthors);
 
   return (
     <PageLayout singlePage>
--- gatsby-theme-test/src/components/PageTag/styles.js
+++ gatsby-theme-test/src/components/PageTag/styles.js
@@ -1,11 +1,6 @@
 import styled from "styled-components";
 import { CONTENT_MAX_WIDTH, CONTENT_OVERLAP_HEIGHT } from "../variables";
 
-export const Header = styled.div`
-  padding: 0 12px;
-  margin: 0 0 -12px;
-`;
-
 export const Content = styled.div`
   display: block;
   margin: -${CONTENT_OVERLAP_HEIGHT}px auto 0;
