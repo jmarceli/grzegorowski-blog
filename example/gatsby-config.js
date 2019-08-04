b0b07a6918514ea091ea3fc929b4d48051100657
Jan Grzegorowski <grzegorowski@gmail.com>
2019-08-05 00:32:04 +0200

    Add favicon

--- example/gatsby-config.js
+++ example/gatsby-config.js
@@ -61,5 +61,36 @@ module.exports = {
         exclude: ["/cookies"],
       },
     },
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: path.join(__dirname, "content", "img", "favicon.png"),
+
+        // WebApp Manifest Configuration
+        // appName: null, // Inferred with your package.json
+        // appDescription: null,
+        // developerName: null,
+        // developerURL: null,
+        // dir: 'auto',
+        // lang: 'en-US',
+        // background: '#fff',
+        // theme_color: '#fff',
+        // display: 'standalone',
+        // orientation: 'any',
+        // start_url: '/?homescreen=1',
+        // version: '1.0',
+
+        // icons: {
+        //   android: true,
+        //   appleIcon: true,
+        //   appleStartup: true,
+        //   coast: false,
+        //   favicons: true,
+        //   firefox: true,
+        //   yandex: false,
+        //   windows: false
+        // }
+      },
+    },
   ],
 };
