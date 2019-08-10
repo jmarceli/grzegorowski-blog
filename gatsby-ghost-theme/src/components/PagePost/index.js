6cc361c0ed2926449568763b1ad31f12fefb3c4f
Jan Grzegorowski <grzegorowski@gmail.com>
2019-08-10 02:17:42 +0200

    Fix post author name

--- gatsby-ghost-theme/src/components/PagePost/index.js
+++ gatsby-ghost-theme/src/components/PagePost/index.js
@@ -80,7 +80,7 @@ export default function PagePost({
 
   return (
     <PageLayout singlePage opaque>
-      <Seo data={post} author={author} />
+      <Seo data={post} author={author.name} />
 
       <Wrapper>
         <Header>
