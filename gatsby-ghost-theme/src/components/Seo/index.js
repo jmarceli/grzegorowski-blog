d9b64d0125aec36c55afe242675c7458d16b2d88
Jan Grzegorowski <grzegorowski@gmail.com>
2020-04-26 23:56:15 +0200

    Add canonical to posts

--- gatsby-ghost-theme/src/components/Seo/index.js
+++ gatsby-ghost-theme/src/components/Seo/index.js
@@ -7,6 +7,17 @@ import {
   schemaArticle,
 } from "../../utils/seo";
 
+const baseUrl = "https://www.grzegorowski.com";
+
+const getCanonicalUrl = (contentType, data) => {
+  if (!contentType) {
+    // post has no contentType assigned
+    return baseUrl + "/" + data.frontmatter.slug;
+  }
+  // TODO: add canonical to other pages
+  return "";
+};
+
 export default function Seo({ data, author, contentType }) {
   const seoData = {
     headline: data.frontmatter.meta_title || data.frontmatter.title,
@@ -19,32 +30,32 @@ export default function Seo({ data, author, contentType }) {
     keywords: data.frontmatter.tags && data.frontmatter.tags.join(", "),
     imageUrl:
       data.frontmatter.feature_image &&
-      "https://grzegorowski.com/" + data.frontmatter.feature_image.relativePath,
+      baseUrl + data.frontmatter.feature_image.relativePath,
     copyrightYear: data.frontmatter.date_created,
     author: author && author.node && author.node.name,
   };
 
   const schema =
     contentType === "website"
-      ? schemaWebsite(
-          { ...seoData, url: "https://www.grzegorowski.com" },
-          seoData.author,
-        )
+      ? schemaWebsite({ ...seoData, url: baseUrl }, seoData.author)
       : contentType === "author"
       ? schemaPerson({
           name: seoData.headline,
           imageUrl: seoData.imageUrl,
-          url: "https://www.grzegorowski.com/" + data.slug,
+          url: baseUrl + data.slug,
         })
       : contentType === "article"
       ? schemaArticle(seoData, seoData.author)
       : schemaBlogPosting(seoData, seoData.author);
+
+  const canonical = getCanonicalUrl(contentType, data);
 
   return (
     <Helmet script={schema}>
       <title>{seoData.headline}</title>
       <meta name="description" content={seoData.description} />
       {seoData.author && <meta name="author" content={seoData.author} />}
+      {canonical && <link rel="canonical" href={canonical} />}
     </Helmet>
   );
 }
